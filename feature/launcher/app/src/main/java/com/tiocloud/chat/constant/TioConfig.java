642e5918922ff94b29083cf42cf10ca61ab7bfe6
anderson <anderson1108@gmail.com>
2022-12-17 11:58:33 +0800

    整理打包配置

--- feature/launcher/app/src/main/java/com/tiocloud/chat/constant/TioConfig.java
+++ feature/launcher/app/src/main/java/com/tiocloud/chat/constant/TioConfig.java
@@ -3,18 +3,14 @@ package com.tiocloud.chat.constant;
 import com.tiocloud.chat.BuildConfig;
 
 public class TioConfig {
-
     public static  final String BASE_URL = "";
-
 
     /**
      * 握手密钥
      */
     public static final String HAND_SHAKE_KEY = "WS52U4uV0pztMu7";
 
-    public static  String OSS_objectName =
-            BuildConfig.FLAVOR.equalsIgnoreCase("forqa")?
-                    "ip/ceshi.json":"ip/ip.json";
+    public static  String OSS_objectName = "ip/"+ BuildConfig.ip+".json";
 
     // 访问的endpoint地址
     public static final String OSS_ENDPOINT = "oss-cn-shenzhen.aliyuncs.com";
